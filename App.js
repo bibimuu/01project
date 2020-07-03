2f07c34992488b3c37a7dd2d01b4a6855bb6c194
平松大悟朗 <h.dai56@gmail.com>
2020-07-03 21:31:10 +0900

    first commit

--- App.js
+++ App.js
@@ -1,12 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.itemContainer}>
+        <View style={styles.leftContainer}>
+          <Image
+            source={{ uri: 'https://picsum.photos/200/200' }}
+            style={styles.image}
+          />
+        </View>
+        <View style={styles.rightContainer}>
+          <Text numberOfLines={3}>
+            lex関係のプロパティは外出しして書いています。 この場合は、
+            ヘッダー：メインコンテンツ：フッターのflexプロパティの値は 0.3:2:0.5
+            なのでこの比率が縦方向長さの比率となります。
+          </Text>
+          <Text>react news</Text>
+        </View>
+      </View>
     </View>
   );
 }
@@ -17,5 +31,22 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  itemContainer: {
+    height: 100,
+    width: '100%',
+    borderColor: 'grey',
+    borderWidth: 1,
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+  },
+  image: {
+    height: '100%',
+    width: '100%',
   },
 });
